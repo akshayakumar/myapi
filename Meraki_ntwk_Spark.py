392a3ef8a6eff7eac9abe29dfcc7e00e417fdfba
Akshay kumar <fun4ax@gmail.com>
2018-03-05 10:03:24 -0700

    Update Meraki_ntwk_Spark.py

--- Meraki_ntwk_Spark.py
+++ Meraki_ntwk_Spark.py
@@ -8,7 +8,7 @@ url = "https://n67.meraki.com/api/v0/organizations/191019/networks"
 
 headers = {
     'content-type': "application/json",
-    'x-cisco-meraki-api-key': "25350e0d351caa70d3e40fee0ce6e9d8b15998ee",
+    'x-cisco-meraki-api-key': "",
     'cache-control': "no-cache"
     }
 
@@ -16,7 +16,7 @@ sparkurl = "https://api.ciscospark.com/v1/messages"
 
 sparkheader = {
     'content-type': "application/json; charset=utf-8",
-    'authorization': "Bearer YzRhNmU1YmUtOGQ5NC00NjcxLTllYWYtNjAzMzVhODg2NjE0NTAxY2ViY2UtMmJm",
+    'authorization': "Bearer ",
     'cache-control': "no-cache"
     }
 
@@ -31,6 +31,6 @@ for network in s:
     print time
     print network ['name']
     print network['id'] + '\n'
-    payload = { 'roomId': "Y2lzY29zcGFyazovL3VzL1JPT00vMWFlYzE5NDAtMzdmMy0xMWU2LWFjZGQtNDk0YjNhOWJlOWY1",
+    payload = { 'roomId': "",
                'text': string}
     spark = requests.request("POST", sparkurl, data=json.dumps(payload), headers=sparkheader)
