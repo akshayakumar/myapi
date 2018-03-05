744702461c75089ed3a41c577e08ee1262401ae1
Akshay kumar <fun4ax@gmail.com>
2018-03-05 10:03:54 -0700

    Update Meraki_ntwk_Spark_Bot.py

--- Meraki_ntwk_Spark_Bot.py
+++ Meraki_ntwk_Spark_Bot.py
@@ -8,16 +8,16 @@ neturl = "https://n67.meraki.com/api/v0/organizations/191019/networks"
 
 netheaders = {
     'content-type': "application/json",
-    'x-cisco-meraki-api-key': "25350e0d351caa70d3e40fee0ce6e9d8b15998ee",
+    'x-cisco-meraki-api-key': "",
     'cache-control': "no-cache"
     }
 
 #URL for Chitti Bot
-sparkurl = "https://api.ciscospark.com/v1/webhooks/incoming/Y2lzY29zcGFyazovL3VzL1dFQkhPT0svZjEzNjM2NDQtYTk5YS00MjdkLWI4YWYtNzhhODk5MThlYWI4"
+sparkurl = "https://api.ciscospark.com/v1/webhooks/incoming/"
 
 sparkheader = {
     'content-type': "application/json; charset=utf-8",
-    'authorization': "Bearer YzRhNmU1YmUtOGQ5NC00NjcxLTllYWYtNjAzMzVhODg2NjE0NTAxY2ViY2UtMmJm",
+    'authorization': "Bearer ",
     'cache-control': "no-cache"
     }
 
@@ -32,6 +32,6 @@ for network in s:
     print time
     print network ['name']
     print network['id'] + '\n'
-    payload = { 'roomId': "Y2lzY29zcGFyazovL3VzL1JPT00vMWFlYzE5NDAtMzdmMy0xMWU2LWFjZGQtNDk0YjNhOWJlOWY1",
+    payload = { 'roomId': "",
                'text': string}
     spark = requests.request("POST", sparkurl, data=json.dumps(payload), headers=sparkheader)
